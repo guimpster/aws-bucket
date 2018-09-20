955a54d27bf59d8b0c33f33b68bb6eab75245e9b
Guilherme Campos <guilherme.campos@kapmug.com.br>
2018-09-20 13:51:49 -0300

    feat(azure): azure face comparision and recognition example

---
+++ src/azure.js
@@ -0,0 +1,52 @@
+import fetch from 'node-fetch';
+
+class Azure {
+  constructor(config) {
+    //super(config);
+
+    this.azureAccess = {
+      url: process.env.AZURE_ACCESS_URL,
+      key1: process.env.AZURE_ACCESS_KEY,
+      key2: process.env.AZURE_ACCESS_KEY,
+      ...config
+    };
+
+    this.accessHeader = {
+        "Ocp-Apim-Subscription-Key": this.azureAccess.key1
+    };
+  }
+
+//   async detectFaceAzure(url) {
+//     const body = { returnFaceLandmarks: false, returnFaceId: true };
+//     return fetch(`${this.azureAccess}/verify`, { 
+//         method: 'POST',
+//         body:    JSON.stringify(body),
+//         headers: { ...this.accessHeader },
+//     }).then(res => res.json());
+//   }
+
+  async detectFaceAzure(url) {
+    return fetch(`${this.azureAccess.url}/detect?returnFaceId=true&returnFaceLandmarks=false`, { 
+        method: 'POST',
+        body: JSON.stringify({ url }),
+        headers: { ...this.accessHeader, "Content-Type": "application/json" },
+    }).then(res => res.json());
+  }
+
+  async verifyFaces(url1, url2) {
+    const [{faceId: faceId1}] = await this.detectFaceAzure(url1);
+    const [{faceId: faceId2}] = await this.detectFaceAzure(url2);
+
+    console.log('Face1 meta: ', require('util').inspect(faceId1, { depth: null }))
+    console.log('Face2 meta: ', require('util').inspect(faceId2, { depth: null }))
+
+    const body = { faceId1, faceId2 };
+    return fetch(`${this.azureAccess.url}/verify`, { 
+        method: 'POST',
+        body:    JSON.stringify(body),
+        headers: { ...this.accessHeader, "Content-Type": "application/json" },
+    }).then(res => res.json());
+  }
+}
+
+export default Azure;
--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ import { ApolloServer } from "apollo-server-express";
 import AWS from "aws-sdk";
 import uuid from "node-uuid";
 
+import Azure from "./azure"
+
 import {
   getSignedUrlPromise,
-  getObject,
   parseStream,
   asyncMiddleware
 } from "./utils";
@@ -18,8 +19,10 @@ import resolvers from "./resolvers";
 import { getUser } from "./faker";
 import typeDefs from "./schema.graphql";
 import awsAccess from "./aws-access.json";
+import azureAccess from "./azure-access.json";
 
 const s3 = new AWS.S3(awsAccess);
+const azure = new Azure(azureAccess);
 const app = express();
 const Server = new ApolloServer({
   schema: applyMiddleware(makeExecutableSchema({ typeDefs, resolvers })),
@@ -46,11 +49,32 @@ app.get(
   })
 );
 
+app.get(
+  "/compareImages",
+  asyncMiddleware(async (req, res, next) => {
+    const face1_params = { Key: "bill_gates_1.jpeg", Bucket: "photos-uploader" };
+    const face2_params = { Key: "bill_gatewa_2.jpg", Bucket: "photos-uploader" };
+
+    const face1_url = await getSignedUrlPromise(s3, "getObject", face1_params);
+    const face2_url = await getSignedUrlPromise(s3, "getObject", face2_params);
+
+    console.log(`Face1's url is ${face1_url}`);
+    console.log(`Face2's url is ${face2_url}`);
+
+    const response = await azure.verifyFaces(face1_url, face2_url);
+
+    console.log('Response: ', require('util').inspect(response, { depth: null }))
+
+    res.json(response);
+  })  
+);
+
 const options = { port: 4000 };
 
 app.listen(options, () => {
   console.log(
     `🚀 Graphql ready at http://localhost:4000${Server.graphqlPath}
-🚀 Server ready at http://localhost:4000/image`
+🚀 Server ready at http://localhost:4000/image
+🚀 Server ready at http://localhost:4000/compareImages`
   );
 });
